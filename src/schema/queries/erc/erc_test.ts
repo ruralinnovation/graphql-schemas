ec78d79d0a85fcbe448a46c03729ade45b36b231
John Hall <john.hall@ruralinnovation.us>
2024-06-12 15:32:25 -0400

    try aws sdk js v3 in erc test query

--- src/schema/queries/erc/erc_test.ts
+++ src/schema/queries/erc/erc_test.ts
@@ -1,11 +1,12 @@
 import { GraphQLObjectType, GraphQLString } from "graphql/type";
+import * as AWS from 'aws-sdk';
 
-// TODO: Add S3 fetch logic...
+import { S3, S3ClientConfig } from "@aws-sdk/client-s3";
 
 const erc_s3_test = {
   // An array (list) fails to transfer gzipped by local sam cli
   type: // new GraphQLList(
-  // ... but a single return object can be gzipped locally
+        // ... but a single return object can be gzipped locally
     new GraphQLObjectType({
       name: 'ERCS3TestObject',
       fields: () => ({
@@ -19,6 +20,39 @@ const erc_s3_test = {
     { dataSources: { pythonApi } }: any,
     info: any
   ) =>  {
+    console.log("dataSources:", [
+      pythonApi
+    ]);
+
+    // TODO: Add S3 fetch logic...
+    console.log("AWS credentials:", {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: process.env.AWS_REGION,
+    });
+    // // TODO: Find a new way to set credentials for S3
+    // // This method of setting creds is deprecated...
+    // // JS SDK v3 does not support global configuration.
+    // // Codemod has attempted to pass values to each service client in this file.
+    // // You may need to update clients outside of this file, if they use global config.
+    // AWS.config.update({
+    //   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    //   region: process.env.AWS_REGION,
+    // });
+
+    const s3_config: S3ClientConfig = {
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+      },
+
+      region: process.env.AWS_REGION,
+    };
+    const s3 = new S3(s3_config);
+
+    // TODO: Try to fetch S3 resource
+    console.log("S3 buckets:", s3.listBuckets);
 
     const value = {
       "erc_s3_test": {
@@ -34,10 +68,9 @@ const erc_s3_test = {
       "description": "Percent of broadband serviceable locations with access to 25/3",
     };
 
-    return [{
+    return [{ // An array (list) fails to transfer gzipped by local sam cli...
         ...value
-    }]      // An array (list) fails to transfer gzipped by local sam cli
-      [0];  // ... but a single return object can be gzipped locally
+    }][0];    // ... but a single return object can be gzipped locally
   }
 };
 
