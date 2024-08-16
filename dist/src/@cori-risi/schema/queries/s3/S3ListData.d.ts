96826ffe037906cce63e8ad0308df53905afb60d
John Hall <john.hall@ruralinnovation.us>
2024-08-16 10:48:41 -0400

    add type for s3 list data

--- dist/src/@cori-risi/schema/queries/s3/S3ListData.d.ts
+++ dist/src/@cori-risi/schema/queries/s3/S3ListData.d.ts
@@ -1,10 +1,6 @@
-export default function S3ListData(Bucket?: string, container_name?: string): Promise<{
-    type: string;
-    list: any;
-    test: {
-        "S3ListData resolver can list objects": boolean;
-        "S3ListData manifest can be encoded as a valid character string": boolean;
-        "S3ListData manifest object is valid JSON": boolean;
-        "S3ListData manifest contains valid list of data": boolean;
-    };
-}>;
+export interface S3DataList {
+    type: "S3DataList";
+    list: any[];
+    test: any;
+}
+export default function S3ListData(Bucket?: string, container_name?: string): Promise<S3DataList>;
--- dist/src/@cori-risi/schema/queries/s3/index.d.ts
+++ dist/src/@cori-risi/schema/queries/s3/index.d.ts
@@ -1,4 +1,4 @@
 declare const s3Queries: any;
 export default s3Queries;
-export { default as S3ListData } from "./S3ListData";
+export { default as S3ListData, S3DataList } from "./S3ListData";
 export { default as S3Test } from "./S3Test";
